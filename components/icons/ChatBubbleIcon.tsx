9414f0c5c53a2202d2434fb20307576b3b8183f6
avi-guha <aviguha05@gmail.com>
2025-11-15 20:57:17 -0800

    what

--- components/icons/ChatBubbleIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const ChatBubbleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.94-.416-1.253l-.5-1.027a9 9 0 0 1-1.06-3.273A9 9 0 0 1 21 12Z" />
-  </svg>
-);
